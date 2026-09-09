0df39a56c52f034d449dc557053ce91fe7206e2b
Paulo Adriano <paulo_adriano2014@live.com>
2026-09-09 17:51:24 -0300

    fix: bibliotecaData aponta pros .png de categoria existentes

--- src/data/bibliotecaData.js
+++ src/data/bibliotecaData.js
@@ -3,16 +3,16 @@
 // empacotados junto com o app. "TODOS" não entra aqui de propósito: já é
 // tratado à parte em BibliotecaAdmin.js (usa elitefit_banner_generic.png).
 export const categoryCovers = {
-    "Peito": require('../../assets/categoria_peito.jpg'),
-    "Costas": require('../../assets/categoria_costas.jpg'),
-    "Pernas": require('../../assets/categoria_pernas.jpg'),
-    "Ombros": require('../../assets/categoria_ombros.jpg'),
-    "Bíceps": require('../../assets/categoria_biceps.jpg'),
-    "Tríceps": require('../../assets/categoria_triceps.jpg'),
-    "Abdômen": require('../../assets/categoria_abdomen.jpg'),
-    "Cardio": require('../../assets/categoria_cardio.jpg'),
-    "Antebraço": require('../../assets/categoria_antebraco.jpg'),
-    "Mobilidade": require('../../assets/categoria_mobilidade.jpg'),
+    "Peito": require('../../assets/categoria_peito.png'),
+    "Costas": require('../../assets/categoria_costas.png'),
+    "Pernas": require('../../assets/categoria_pernas.png'),
+    "Ombros": require('../../assets/categoria_ombros.png'),
+    "Bíceps": require('../../assets/categoria_biceps.png'),
+    "Tríceps": require('../../assets/categoria_triceps.png'),
+    "Abdômen": require('../../assets/categoria_abdomen.png'),
+    "Cardio": require('../../assets/categoria_cardio.png'),
+    "Antebraço": require('../../assets/categoria_antebraco.png'),
+    "Mobilidade": require('../../assets/categoria_mobilidade.png'),
 };
 
 export const categories = [
